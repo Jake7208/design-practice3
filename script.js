94bb823dfb82ed8a172dae9eee35e6f0d235ad40
Jake7208 <jake7208@gmail.com>
2023-09-06 19:48:18 -0600

    update circle section is complete

--- script.js
+++ script.js
@@ -25,12 +25,39 @@ const videos = document.querySelector('.video');
         delay: .5
     })
 
-    const mockup = document.getElementById('mockups');
-    // Pin the element initially
+    let sticky = document.querySelector('.sticky-container');
+    let mockup = document.querySelector('mockup-container');
+    const testimonials = document.querySelector('.testimonials');
 
-
-
+    // sticky circle
+    let tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: mockup,
+            start: 'center+=200 bottom',
+            end: testimonials,
+            pin: true,
+            markers: true,
+            scrub: 2
+        }
+    });
     
+    tl.fromTo(sticky, // specify the target element here
+        {
+            width: '35em',
+            height: '35em',
+            borderRadius: '35em',
+            backgroundColor: 'black',
+            zIndex: -1
+        },
+        {
+            width: '100%',
+            height: '100%',
+            borderRadius: '0',
+            backgroundColor: 'white',
+            zIndex: 1,
+            duration: 3
+        }
+    );
 
 const lenis = new Lenis()
 
